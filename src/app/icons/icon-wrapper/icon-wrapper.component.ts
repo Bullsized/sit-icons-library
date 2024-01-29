2b3cadf97dbb797f2ead2b65f8fad75e902b32f2
Ivan Stefanov <ivanstefanovbg@gmail.com>
2024-01-29 17:51:54 +0200

    initial set up of the search form control

--- src/app/icons/icon-wrapper/icon-wrapper.component.ts
+++ src/app/icons/icon-wrapper/icon-wrapper.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IconsService } from '../../core/services/icons.service';
 import { Icon } from '../../core/models/Icon';
 import { Category } from '../../core/models/Category';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'sit-icon-wrapper',
@@ -10,17 +12,34 @@ import { Category } from '../../core/models/Category';
 })
 export class IconWrapperComponent implements OnInit {
   allIcons: Icon[] = [];
+  filteredIcons: Icon[] = [];
   uniqueCategories: Category[] = [];
+  searchFC: FormControl<string | null> = new FormControl<string | null>(null);
 
   constructor(private iconsService: IconsService) {}
 
   ngOnInit(): void {
     this.allIcons = this.iconsService.getAllIcons();
+    this.filteredIcons = this.allIcons;
     this.uniqueCategories = [
       ...new Set(this.allIcons.map((icon) => icon.category)),
     ] as Category[];
 
     console.log(this.allIcons);
     console.log(this.uniqueCategories);
+
+    this.searchFC.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((searchValue) => {
+        if (searchValue === null) return;
+
+        this.filterIconsOnInput(searchValue);
+      });
+  }
+
+  filterIconsOnInput(keyword: string): void {
+    this.filteredIcons = this.allIcons.filter(icon =>
+      icon.keywords.some(k => k.toLowerCase().includes(keyword.toLowerCase()))
+    );
   }
 }
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -4,6 +4,9 @@ import { IconDialogComponent } from './icon-dialog/icon-dialog.component';
 import { IconWrapperComponent } from './icon-wrapper/icon-wrapper.component';
 import { IconListComponent } from './icon-list/icon-list.component';
 import { IconRandomComponent } from './icon-random/icon-random.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -13,6 +16,6 @@ import { IconRandomComponent } from './icon-random/icon-random.component';
     IconRandomComponent,
   ],
   exports: [IconWrapperComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormField, MatInputModule, ReactiveFormsModule],
 })
 export class IconsModule {}
