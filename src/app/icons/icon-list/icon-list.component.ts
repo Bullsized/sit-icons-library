c782886576ed2bfb1f7b59450248d1e199d7fb52
Ivan Stefanov <ivanstefanovbg@gmail.com>
2024-01-29 17:51:29 +0200

    delegates the listing to the icon-list component

--- src/app/icons/icon-list/icon-list.component.ts
+++ src/app/icons/icon-list/icon-list.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Icon } from '../../core/models/Icon';
+import { Category } from '../../core/models/Category';
 
 @Component({
   selector: 'sit-icon-list',
   templateUrl: './icon-list.component.html',
-  styleUrl: './icon-list.component.css'
+  styleUrl: './icon-list.component.scss',
 })
 export class IconListComponent {
-
+  @Input() icons: Icon[] = [];
+  @Input() iconCategory: Category | undefined;
 }
