159537d3569a80c6a2af56a6149bbdaae1a5551b
Ivan Stefanov <ivanstefanovbg@gmail.com>
2024-01-30 22:35:07 +0200

    completes the icon-dialog.component and adds extra css classes

--- src/app/icons/icon-dialog/icon-dialog.component.spec.ts
+++ src/app/icons/icon-dialog/icon-dialog.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IconDialogComponent } from './icon-dialog.component';
 
 describe('IconDialogComponent', () => {
@@ -8,10 +7,9 @@ describe('IconDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [IconDialogComponent]
-    })
-    .compileComponents();
-    
+      declarations: [IconDialogComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(IconDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/icons/icon-dialog/icon-dialog.component.ts
+++ src/app/icons/icon-dialog/icon-dialog.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IconDialogData } from '../../core/models/Icon';
+import { transformIconName } from '../../core/helpers/helpers';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DownloadService } from '../../core/services/download.service';
 
 @Component({
   selector: 'sit-icon-dialog',
   templateUrl: './icon-dialog.component.html',
-  styleUrl: './icon-dialog.component.css',
+  styleUrl: './icon-dialog.component.scss',
 })
-export class IconDialogComponent {}
+export class IconDialogComponent implements OnInit {
+  transformedIconName: string = '';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: IconDialogData,
+    private dialogRef: MatDialogRef<IconDialogComponent>,
+    private snackBar: MatSnackBar,
+    private clipboard: Clipboard,
+    private downloadService: DownloadService
+  ) {}
+
+  ngOnInit(): void {
+    this.transformedIconName = transformIconName(this.data.icon.name);
+  }
+
+  copyToClipboard(): void {
+    this.clipboard.copy(this.transformedIconName);
+    this.dialogRef.close();
+    this.snackBar.open('Icon name copied to clipboard!', undefined, { duration: 3000 });
+  }
+
+  downloadIcon(): void {
+    this.downloadService.downloadIcon(this.data.icon);
+    this.dialogRef.close();
+    this.snackBar.open('Icon download initiated!', undefined, { duration: 3000 });
+  }
+}
